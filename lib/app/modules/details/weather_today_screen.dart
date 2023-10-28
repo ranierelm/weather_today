e704353c38932b10d897cb35586e652df77c4de5
Raniere Lima <raniere.lima@operalabs.co>
2023-10-28 16:28:11 -0300

    created the weather today screen details

---
+++ lib/app/modules/details/weather_today_screen.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:weather_today/app/modules/models/state_weather_forecast.dart';
+
+import 'package:weather_today/app/modules/models/weather_forecast.dart';
+import 'package:weather_today/app/services/api_service.dart';
+import 'package:weather_today/app/shared/components/background_scaffold.dart';
+import 'package:weather_today/app/shared/components/custom_alert_dialog.dart';
+import 'package:weather_today/app/shared/components/custom_appbar.dart';
+import 'package:weather_today/app/shared/components/states_weather_carousel.dart';
+import 'package:weather_today/app/shared/components/weather_by_time_of_day_card.dart';
+import 'package:weather_today/app/shared/utils/formats.dart';
+import 'package:weather_today/app/shared/utils/weather_utils.dart';
+
+class WeatherTodayScreen extends StatefulWidget {
+  final String state;
+  final WeatherForecast weatherToday;
+  const WeatherTodayScreen({
+    Key? key,
+    required this.state,
+    required this.weatherToday,
+  }) : super(key: key);
+
+  @override
+  State<WeatherTodayScreen> createState() => _WeatherTodayScreenState();
+}
+
+class _WeatherTodayScreenState extends State<WeatherTodayScreen> {
+  String dateToday = '';
+
+  List<StateWeatherForecast> weekForecast = [];
+
+  _getDateToday() {
+    final now = DateTime.now();
+    final formatter = DateFormat("d 'de' MMMM 'de' y", 'pt_BR');
+
+    dateToday = formatter.format(now);
+  }
+
+  bool _isLoading = false;
+
+  Future<void> fetchStateWeatherForecasts() async {
+    setState(() => _isLoading = true);
+    try {
+      weekForecast = await WeatherAPI().getStateWeatherForecasts();
+
+      removeStateWeatherForecastByName(widget.state);
+
+      setState(() {});
+    } catch (error) {
+      debugPrint(error.toString());
+      if (!mounted) return;
+
+      CustomDialog.showAlertDialog(context,
+          title: 'Atenção',
+          content:
+              'Não foi possível buscar atualizações do tempo, tente novamente mais tarde');
+    }
+    setState(() => _isLoading = false);
+  }
+
+  void removeStateWeatherForecastByName(String stateName) {
+    weekForecast.removeWhere((forecast) => forecast.estado == stateName);
+  }
+
+  @override
+  void initState() {
+    _getDateToday();
+    fetchStateWeatherForecasts();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BackgroundScaffold(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: CustomAppBar(
+          title: widget.state,
+          onTapLeading: () => Navigator.pop(context),
+          leadingSemanticsLabel: 'Button to return to the previous page',
+          iconLeading: 'assets/icons/arrow-left-thin.svg',
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Text(
+              FormatUtils.capitalize(widget.weatherToday.dia),
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Color(0xFFDEDDDD),
+                fontSize: 12,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 18),
+            Container(
+              constraints: const BoxConstraints(maxHeight: 125, maxWidth: 160),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(WeatherUtils.getWeatherImageUrl(
+                      WeatherUtils.getDayPeriodWeather(
+                          weekForecast: widget.weatherToday))),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            Column(
+              children: [
+                SizedBox(
+                  height: 95,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        WeatherUtils.getDayPeriodDegrees(
+                            weekForecast: widget.weatherToday),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 81,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Container(
+                        width: 16,
+                        height: 16,
+                        margin: const EdgeInsets.only(top: 10),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(width: 3, color: Colors.white),
+                        ),
+                        // margin: const EdgeInsets.only(bottom: 50),
+                        alignment: Alignment.topCenter,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  dateToday,
+                  style: const TextStyle(
+                    color: Color(0xFFDEDDDD),
+                    fontSize: 12,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 63),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Hoje',
+                    style: TextStyle(
+                      color: Color(0xFFDEDDDD),
+                      fontSize: 12,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      WeatherByTimeOfDayCard(
+                        time: 'Manhã',
+                        weatherForecast: widget.weatherToday.manha,
+                      ),
+                      WeatherByTimeOfDayCard(
+                        time: 'Tarde',
+                        weatherForecast: widget.weatherToday.tarde,
+                      ),
+                      WeatherByTimeOfDayCard(
+                        time: 'Noite',
+                        weatherForecast: widget.weatherToday.noite,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 44),
+            _isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : StatesWeatherCarousel(
+                    weatherByStates: weekForecast,
+                  ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
