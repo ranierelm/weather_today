b1283ea86b1e316d53d3db0bb2a67135da61b202
Raniere Lima <raniere.lima@operalabs.co>
2023-10-28 10:54:39 -0300

    created card that shows the weather by day of the week

--- lib/app/shared/components/temperature_days_week_card.dart
+++ lib/app/shared/components/temperature_days_week_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_today/app/shared/utils/formats.dart';
 
 class TemperatureDaysWeekCard extends StatelessWidget {
   final String dayWeek;
@@ -19,67 +20,80 @@ class TemperatureDaysWeekCard extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            dayWeek,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.w500,
+          Expanded(
+            flex: 3,
+            child: Text(
+              FormatUtils.capitalize(dayWeek),
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ),
-          Row(
-            children: [
-              Container(
-                constraints: const BoxConstraints(maxHeight: 50, maxWidth: 60),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(weatherUrl),
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Text(
-                weatherName,
-                style: const TextStyle(
-                  color: Colors.white60,
-                  fontSize: 16,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w500,
-                ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 30,
+          Expanded(
+            flex: 5,
             child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  '${double.parse(temperature) > 0 ? '+' : '-'} $temperature',
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
                 Container(
-                  width: 5,
-                  height: 5,
+                  constraints:
+                      const BoxConstraints(maxHeight: 50, maxWidth: 60),
                   decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(width: 1, color: Colors.white),
+                    image: DecorationImage(
+                      image: AssetImage(weatherUrl),
+                      fit: BoxFit.contain,
+                    ),
                   ),
-                  margin: const EdgeInsets.only(bottom: 20, left: 2),
-                  alignment: Alignment.topCenter,
                 ),
+                const SizedBox(width: 5),
+                Flexible(
+                  child: Text(
+                    weatherName,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      color: Colors.white60,
+                      fontSize: 16,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                )
               ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: SizedBox(
+              height: 30,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    temperature,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Container(
+                    width: 5,
+                    height: 5,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(width: 1, color: Colors.white),
+                    ),
+                    margin: const EdgeInsets.only(bottom: 20, left: 2),
+                    alignment: Alignment.topCenter,
+                  ),
+                ],
+              ),
             ),
           )
         ],
